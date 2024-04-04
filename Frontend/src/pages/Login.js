3ee9d1d3c0cf93263f1d3965be5c2927285ef8df
unknown <hegdehari256@gmail.com>
2024-04-04 07:22:51 +0530

    done

---
+++ Frontend/src/pages/Login.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const LoginPage = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:5000/api/user/login", formData);
+      localStorage.setItem("token", response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ Frontend/src/pages/Signup.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const SignupPage = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    age: "",
+    gender: "",
+    phone: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/api/user/signup", formData);
+    } catch (error) {
+      console.error(error);
+      // Handle error
+    }
+  };
+
+  return (
+    <div>
+      <h2>Signup</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="age"
+          placeholder="Age"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="gender"
+          placeholder="Gender"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="phone"
+          placeholder="Phone"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignupPage;
